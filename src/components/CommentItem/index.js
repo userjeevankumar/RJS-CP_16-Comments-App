992248624fed28d51dc1755ae082302de6fcf593
userjeevankumar <jeevankmrjk@gamil.com>
2023-05-27 16:12:28 +0000

    solution

---
+++ src/components/CommentItem/index.js
@@ -0,0 +1,68 @@
+// Write your code here
+import {formatDistanceToNow} from 'date-fns'
+import './index.css'
+
+const CommentItem = props => {
+  const {commentDetails} = props
+  const {id, name, comment, isLiked, initialClassName, data} = commentDetails
+  const initial = name ? name[0].toUpperCase() : ''
+  const likeTextClassName = isLiked ? 'button active' : 'button'
+  const likeImageUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+  const postedTime = formatDistanceToNow(data)
+
+  const onClickLike = () => {
+    const {toggleIsLiked} = props
+    toggleIsLiked(id)
+  }
+
+  const onDeleteComment = () => {
+    const {deleteComment} = props
+    deleteComment(id)
+  }
+
+  return (
+    <li>
+      <div>
+        <div className={initialClassName}>
+          <p>{initial}</p>
+        </div>
+        <div>
+          <div>
+            <p>{name}</p>
+            <p>{postedTime} ago</p>
+          </div>
+          <p>{comment}</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src={likeImageUrl} alt="like" className="like-img" />
+          <button
+            className={likeTextClassName}
+            type="button"
+            onClick={onClickLike}
+          >
+            Like
+          </button>
+        </div>
+        <button
+          className="button"
+          type="button"
+          onClick={onDeleteComment}
+          data-testid="delete"
+        >
+          <img
+            className="delete"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+      <hr className="comment-line" />
+    </li>
+  )
+}
+
+export default CommentItem
